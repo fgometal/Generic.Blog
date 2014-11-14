1f02abafb807eeb3df64231b0ec0811f4a960b47
fgometal <fgometal@gmail.com>
2014-11-14 15:42:09 -0200

    Criação do Contexto e

---
+++ MundiPagg.Blog.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MundiPagg.Blog.Domain.Entities
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AboutMe { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime DateRegistered { get; set; }
+    }
+}
---
+++ MundiPagg.Blog.Repository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MundiPagg.Blog.Domain.Entities;
+using MundiPagg.Blog.Repository.Interfaces;
+using MundiPagg.Blog.Repository.EFDbContext;
+
+namespace MundiPagg.Blog.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private BlogDBContext context = new BlogDBContext();
+
+        public IQueryable<User> Users
+        {
+            get { return context.Users; }
+        }
+    }
+}
