0bb0e5e9affab73cda881193abfc4146ccbd4760
fgometal <fgometal@gmail.com>
2014-11-14 13:25:35 -0200

    Criação do repositório
    - Criação do repositório.
    - Criação dos projetos (camadas) da aplicação.

---
+++ MundiPagg.Blog.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,34 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MundiPagg.Blog.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+        public class NinjectControllerFactory : DefaultControllerFactory
+        {
+            private IKernel ninjectKernel;
+            public NinjectControllerFactory()
+            {
+                ninjectKernel = new StandardKernel();
+                AddBindings();
+            }
+            protected override IController GetControllerInstance(RequestContext requestContext,
+            Type controllerType)
+            {
+                return controllerType == null
+                ? null
+                : (IController)ninjectKernel.Get(controllerType);
+            }
+            private void AddBindings()
+            {
+                // put additional bindings here
+            }
+        }
+    }
+}
