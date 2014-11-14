db14a3e61f74978a8f57644ef24125dbbffb5e60
fgometal <fgometal@gmail.com>
2014-11-14 15:43:12 -0200

    Revert "Revert "Criação do Contexto e""
    This reverts commit 5c752619a5eff3d7ef39e089c3042122575f1614.

---
+++ MundiPagg.Blog.Repository/EFDbContext/BlogDBContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.Entity;
+using MundiPagg.Blog.Domain.Entities;
+
+namespace MundiPagg.Blog.Repository.EFDbContext
+{
+    public class BlogDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ MundiPagg.Blog.Repository/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MundiPagg.Blog.Domain.Entities;
+
+namespace MundiPagg.Blog.Repository.Interfaces
+{
+    public interface IUserRepository
+    {
+        IQueryable<User> Users { get; }
+
+        void Save(User user);
+        void Update(User user);
+        void Delete(User user);
+    }
+}
